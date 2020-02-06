f4b999c4e0f938db9a1cd5b5bb73af01f231ae0b
Brian Nicholls <nichol88@gmail.com>
2020-02-06 13:08:20 -0500

    updated environment, add database

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,14 @@
+ENV['SINATRA_ENV'] ||= 'development'
+
 # require everything in the Gemfile
 require 'bundler/setup'
 Bundler.require
 
 # establish connection to db
-
+set :database, {
+  adapter: 'sqlite3',
+  database: "db/#{ENV['SINATRA_ENV']}.sqlite3"
+}
 
 # require local files
 require_all 'app'
---
+++ db/migrate/20200206180307_create_dogs.rb
@@ -0,0 +1,10 @@
+class CreateDogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.integer :age
+      t.string :breed
+      t.timestamps null: false
+    end
+  end
+end
